bcd765f3fa6e4563da7be688375baf5949904e07
syb0rg <darksyb0rg@gmail.com>
2014-07-02 20:23:22 -0500

    Removed header file.

--- main.c
+++ main.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "UTTT.h"
+
+#define ROWS 9
+#define COLS 9
+
+typedef char Board[ROWS][COLS];
+typedef char MetaBoard[ROWS / 3][COLS / 3];
+typedef enum {VALID, NOT_A_DIGIT, NOT_IN_BOARD, SPACE_OCCUPIED, OUT_OF_BOUNDS} MoveStatus;
+static const char marks[] = {'O', 'X'};
+
+void fillSubBoard(Board board, int x, int y, char c);
+int getBound(int in);
+void printBoard(Board board);
+static int checkMeta(MetaBoard meta);
+static int checkBoard(Board board, MetaBoard meta, int player, int row, int column);
+MoveStatus validCoords(Board board, int row, int column, int rowBound, int columnBound);
 
 void fillSubBoard(Board board, int x, int y, char c)
 {
